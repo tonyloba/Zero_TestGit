933d343c7854a846ec20919586a4a72ad139ee0c
Anton Lobarev <74120447+tonyloba@users.noreply.github.com>
2025-03-25 00:46:37 -0400

    Created Dev2 and Upadated main.py

--- main.py
+++ main.py
@@ -21,3 +21,16 @@ def export_file(file_path, content):
         return "Файл успешно сохранён."
     except Exception as e:
         return f"Произошла ошибка: {e}"
+
+# Created In Dev 2 Branch:
+
+def count_lines_in_file(file_path):
+    """Подсчитывает количество строк в текстовом файле."""
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        return len(lines)
+    except FileNotFoundError:
+        return "Файл не найден."
+    except Exception as e:
+        return f"Произошла ошибка: {e}"
